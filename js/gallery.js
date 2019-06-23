c932c3df1079fec69612943ba19ad74271f81006
Nato Boram <nato.boram+git@gmail.com>
2019-06-23 02:19:15 -0400

    comments

--- js/gallery.js
+++ js/gallery.js
@@ -1,37 +1,73 @@
 const gallery = {
+
+	/**
+	 * Entry point of the gallery.
+	 */
 	start: function () {
 		gallery.render();
-		gallery.pin();
+		gallery.ipfs_ready();
 	},
 
-	render: async function () {
+	/**
+	 * Fetch JSON resources using HoganJS, then display it.
+	 */
+	render: function () {
 		fetch("./json/gallery.json").then(response => response.json()).then(json => {
 			json.galleries.forEach((element, index) => {
-				fetch("https://" + element.base32 + ".cf-ipfs.com/" + element.text).then(response => response.text()).then(text => {
-					json.galleries[index].text = gallery.md.render(text);
+				if (element.text !== "")
+					fetch("https://" + element.base32 + ".cf-ipfs.com/" + element.text).then(response => response.text()).then(text => {
+						json.galleries[index].text = gallery.md.render(text);
+						document.querySelector("#gallery").innerHTML = templates.gallery.render(json);
+					});
+				else {
 					document.querySelector("#gallery").innerHTML = templates.gallery.render(json);
-				});
+				}
 			});
 		});
 	},
 
-	pin: function () {
+	/**
+	 * Setup a local IPFS node.
+	 * 1. Connect to LineageOSOnIPFS.com's swarm
+	 * 2. Add this gallery's pins to the local node
+	 */
+	ipfs_ready: function () {
 		gallery.ipfs.on("ready", () => {
+
+			// IPFS Swarm Connect
+			gallery.ipfs.swarm.connect("/ip6/2604:a880:cad:d0::17:2001/tcp/4001/ipfs/QmSqLAXiJiteNbuNPY4Y5Lp4iKiUmqhCkBZSedZEutktVs", {
+				default: true
+			}, function (err) {
+				console.log(err);
+			});
+			gallery.ipfs.swarm.connect("/ip4/159.89.116.13/tcp/4001/ipfs/QmSqLAXiJiteNbuNPY4Y5Lp4iKiUmqhCkBZSedZEutktVs", {
+				default: true
+			}, function (err) {
+				console.log(err);
+			});
+
+			// IPFS Pin Add
 			fetch("./json/gallery.json").then(response => response.json()).then(json => {
 				json.galleries.forEach(element => {
-					console.log("ipfs pin add " + element.base32);
 					gallery.ipfs.pin.add(element.base32, function (err, res) {
 						console.log(err, res);
 					});
 				});
 			});
 		});
-	}
+	},
 };
 
-// Initialize libraries
-gallery.md = window.markdownit();
+// Markdown
+gallery.md = window.markdownit({
+	xhtmlOut: true,
+	breaks: true,
+	linkify: true,
+	typographer: true,
+});
+
+// JS-IPFS
 gallery.ipfs = new Ipfs();
 
-// Initialize application
+// Start
 gallery.start();
