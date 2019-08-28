9d9f2f480fa20b5db4ed9e1e0f3a5f6a79579f0e
Nato Boram <nato.boram@gmail.com>
2019-08-28 00:42:04 -0400

    moved scripts

--- scripts/ipfs.sh
+++ scripts/ipfs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+./scripts/hulk.sh
+
 # Bootstrap
 ipfs add --wrap-with-directory --chunker=rabin --cid-version=1 https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css.map
 ipfs add --wrap-with-directory --chunker=rabin --cid-version=1 https://code.jquery.com/jquery-3.3.1.slim.min.js
