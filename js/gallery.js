fb252e51739bb00fc009ad928364ca572a6400c8
Nato Boram <nato.boram@gmail.com>
2019-08-27 20:05:59 -0400

    remove js-ipfs

--- js/gallery.js
+++ js/gallery.js
@@ -5,7 +5,6 @@ const gallery = {
 	 */
 	start: function () {
 		gallery.render();
-		gallery.ipfs_ready();
 	},
 
 	/**
@@ -25,37 +24,6 @@ const gallery = {
 			});
 		});
 	},
-
-	/**
-	 * Setup a local IPFS node.
-	 * 1. Connect to LineageOSOnIPFS.com's swarm
-	 * 2. Add this gallery's pins to the local node
-	 */
-	ipfs_ready: function () {
-		gallery.ipfs.on("ready", () => {
-
-			// IPFS Swarm Connect
-			gallery.ipfs.swarm.connect("/ip6/2604:a880:cad:d0::17:2001/tcp/4001/ipfs/QmSqLAXiJiteNbuNPY4Y5Lp4iKiUmqhCkBZSedZEutktVs", {
-				default: true
-			}, function (err) {
-				console.log(err);
-			});
-			gallery.ipfs.swarm.connect("/ip4/159.89.116.13/tcp/4001/ipfs/QmSqLAXiJiteNbuNPY4Y5Lp4iKiUmqhCkBZSedZEutktVs", {
-				default: true
-			}, function (err) {
-				console.log(err);
-			});
-
-			// IPFS Pin Add
-			fetch("./json/gallery.json").then(response => response.json()).then(json => {
-				json.galleries.forEach(element => {
-					gallery.ipfs.pin.add(element.cidv1, function (err, res) {
-						console.log(err, res);
-					});
-				});
-			});
-		});
-	},
 };
 
 // Markdown
@@ -65,9 +33,6 @@ gallery.md = window.markdownit({
 	linkify: true,
 	typographer: true,
 });
-
-// JS-IPFS
-gallery.ipfs = new Ipfs();
 
 // Start
 gallery.start();
