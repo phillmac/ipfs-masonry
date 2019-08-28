77addc33292bfd93d5e485103765024e14c64574
Nato Boram <nato.boram@gmail.com>
2019-08-27 20:05:28 -0400

    chmod +x

--- ipfs.sh
+++ ipfs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ipfs add -wr --chunker=rabin css images js json templates index.html robots.txt
+ipfs add --recursive --wrap-with-directory --chunker=rabin --cid-version=1 css images js json templates index.html robots.txt
