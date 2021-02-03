2ec25b0a9cf860a77b38d88727716f4a8da6e180
phillmac <phillmac@users.noreply.github.com>
2021-02-04 09:35:59 +1100

    Disable pinning by default

--- scripts/ipfs.sh
+++ scripts/ipfs.sh
@@ -3,4 +3,9 @@
 ./scripts/hulk.sh
 
 # IPFS Masonry
-ipfs --api=/dns4/ipfs/tcp/5001 add --recursive --wrap-with-directory --chunker=rabin --cid-version=1 css images js templates index.html robots.txt
+if [[ "${IPFS_PIN}" == "TRUE" ]] || [[ "${IPFS_PIN}" == "true" ]]
+then
+    ipfs --api=/dns4/ipfs/tcp/5001 add --recursive --wrap-with-directory --chunker=rabin --cid-version=1 css images js templates index.html robots.txt
+else
+    ipfs --api=/dns4/ipfs/tcp/5001 add --recursive --wrap-with-directory --chunker=rabin --cid-version=1 --pin false css images js templates index.html robots.txt
+fi
