37587b1e97003cccdd6f82fc525637444a9e6f75
phillmac <phillmackintosh@gmail.com>
2021-03-26 01:04:12 +0000

    Fix sh syntax

--- scripts/ipfs.sh
+++ scripts/ipfs.sh
@@ -3,7 +3,7 @@
 ./scripts/hulk.sh
 
 # IPFS Masonry
-if [[ "${IPFS_PIN}" == "TRUE" ]] || [[ "${IPFS_PIN}" == "true" ]]
+if [ "${IPFS_PIN}" == "TRUE" ] || [ "${IPFS_PIN}" == "true" ]
 then
     ipfs --api=/dns4/ipfs/tcp/5001 add --recursive --wrap-with-directory --chunker=rabin --cid-version=1 css images js templates index.html robots.txt
 else
