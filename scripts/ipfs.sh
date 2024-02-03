be1c6e59a8b3e979d2cda0abdd2d8ca6999d7339
phillmac <phillmackintosh@gmail.com>
2024-02-03 08:39:34 +0000

    Fix ignore

--- scripts/ipfs.sh
+++ scripts/ipfs.sh
@@ -5,7 +5,7 @@
 # IPFS Masonry
 if [ "${IPFS_PIN}" = "TRUE" ] || [ "${IPFS_PIN}" = "true" ]
 then
-    ipfs --api=/dns4/ipfs/tcp/5001 add "${@}" --recursive --wrap-with-directory --chunker=rabin --cid-version=1 --exclude 'assets/gallery.png' app assets images templates index.html robots.txt
+    ipfs --api=/dns4/ipfs/tcp/5001 add "${@}" --recursive --wrap-with-directory --chunker=rabin --cid-version=1 --ignore 'assets/gallery.png' app assets images templates index.html robots.txt
 else
-    ipfs --api=/dns4/ipfs/tcp/5001 add "${@}" --recursive --wrap-with-directory --chunker=rabin --cid-version=1 --pin=false --exclude 'assets/gallery.png' app assets images templates index.html robots.txt
+    ipfs --api=/dns4/ipfs/tcp/5001 add "${@}" --recursive --wrap-with-directory --chunker=rabin --cid-version=1 --pin=false --ignore 'assets/gallery.png' app assets images templates index.html robots.txt
 fi
